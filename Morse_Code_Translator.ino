9ee657938a6fe392ce3f39dbb2fdba1fc70df245
angelina-tsuboi <angelina.t1832@gmail.com>
2020-10-31 08:52:57 -0700

    getting input from buttons works

--- Morse_Code_Translator.ino
+++ Morse_Code_Translator.ino
@@ -2,30 +2,35 @@
 const int dashLED = 2;
 const int dotLED = 3;
 const int buzzerPin = 4;
-//const int centerButton = 5;
-//const int rightButton = 6;
-//const int leftButton = 7;
+const int rightButton = 5;
+const int centerButton = 6;
+const int leftButton = 7;
 //const int potPin = A3;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(dotLED, OUTPUT);
   pinMode(dashLED, OUTPUT);
-//  pinMode(centerButton, INPUT);
-//  pinMode(rightButton, INPUT);
-//  pinMode(leftButton, INPUT);
+  pinMode(centerButton, INPUT);
+  pinMode(rightButton, INPUT);
+  pinMode(leftButton, INPUT);
 //  pinMode(potPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(dotLED, HIGH);
-  digitalWrite(dashLED, LOW);
-  tone(buzzerPin, 1000);
-  delay(1000);
-  digitalWrite(dotLED, LOW);
-  digitalWrite(dashLED, HIGH);
-  noTone(buzzerPin);
-  delay(1000);
+//  digitalWrite(dotLED, HIGH);
+//  digitalWrite(dashLED, LOW);
+//  tone(buzzerPin, 1000);
+//  delay(1000);
+//  digitalWrite(dotLED, LOW);
+//  digitalWrite(dashLED, HIGH);
+//  noTone(buzzerPin);
+//  delay(1000);
+  if(digitalRead(leftButton)){
+    digitalWrite(dotLED, HIGH);
+  }else{
+    digitalWrite(dotLED, LOW);
+  }
 }
