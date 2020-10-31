c78bdce607ca8948b7984011a19f0db37a1f961e
angelina-tsuboi <angelina.t1832@gmail.com>
2020-10-30 22:07:32 -0700

    established variables

--- Morse_Code_Translator.ino
+++ Morse_Code_Translator.ino
@@ -1,6 +1,21 @@
+const int dotLED = 2;
+const int dashLED = 3;
+const int buzzerPin = 4;
+const int centerButton = 5;
+const int rightButton = 6;
+const int leftButton = 7;
+const int potPin = A3;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(dotLED, OUTPUT);
+  pinMode(dashLED, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(centerButton, INPUT);
+  pinMode(rightButton, INPUT);
+  pinMode(leftButton, INPUT);
+  pinMode(potPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
